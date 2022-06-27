991ea813732208f838b4202e4bb84ea30a4768a0
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2022-06-27 20:39:53 -0300

    Introdução ao Try / Catch.

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -6,11 +6,43 @@ using System.Threading.Tasks;
 
 namespace ByteBank
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            try
+            {
+                Metodo();
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Ocorreu um Erro Interno!");
+            }
             Console.ReadLine();
+        }
+
+        private static void Metodo()
+        {
+            TestaDivisao(2);
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                int resultado = Dividir(10, divisor);
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
         }
     }
 }
